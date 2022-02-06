8a554b1566d1ba0110040fb0873607364eb31aa2
SolanaO <silviaEonofrei@gmail.com>
2022-02-06 11:40:53 -0500

    Add sklearn line.

--- scripts/emr_bootstrap_modeling.sh
+++ scripts/emr_bootstrap_modeling.sh
@@ -13,9 +13,11 @@ pip3 install numpy==1.21.2 -v
 pip3 freeze
 pip3 install pandas==1.3.3  -v
 pip3 freeze
-pip3 install matplotlib==3.4.3
+pip3 install matplotlib==3.4.3 -v
 pip3 freeze
 pip3 install seaborn==0.11.2 -v
+pip3 freeze
+pip3 install sklearn==0.0 -v
 pip3 freeze
 
 PYSPARK_PYTHON=/home/hadoop/path/to/venv/bin/python3
